3ec8b2ea03745e45155d12369c73ad879af98414
root <shareisland.org@gmail.com>
2022-02-13 19:15:56 +0100

    IT language edit

--- lang/it/graveyard.php
+++ lang/it/graveyard.php
@@ -15,7 +15,7 @@ return [
     'dead'               => 'Morto',
     'graveyard'          => 'Cimitero',
     'guidelines'         => 'Linee guida',
-    'guidelines-content' => '1) Non puoi resuscitare i tuoi caricamenti. <br> 2) Non resuscitare qualcosa che non puoi impegnare troppo.',
+    'guidelines-content' => '1) Non puoi resuscitare i file che hai tu stesso condiviso. <br> 2) Non resuscitare qualcosa che non puoi condividere a lungo.',
     'howto'              => 'Ecco la regola',
     'howto-desc1'        => 'Devi seminare <span class="text-green"><em>:name</em></span> per <span class="text-red text-bold">30 giorni</span> per una risurrezione di successo. Nel qual caso, quando il vostro seedtime attuale di',
     'howto-desc2'        => 'Sarai ricompensato',
